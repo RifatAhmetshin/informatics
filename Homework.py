d4033267464e699fdc629491c705dbac09894f0d
RifatAhmetshin <rifatasatlenar@gmail.com>
2023-12-16 20:09:46 +0300

    Home work 9

--- Homework.py
+++ Homework.py
@@ -1,155 +1,174 @@
-
-Напишите функцию read_last(lines, file), которая будет открывать определенный файл file и выводить на печать построчно последние строки в количестве lines (на всякий случай проверим, что задано положительное целое число). Протестируем функцию на файле article.txt со следующим содержимым
-
-def read_last(lines, file):
-    try:
-        lines = int(lines)
-        if lines <= 0:
-            print("Количество строк должно быть положительным числом.")
-            return
-    except ValueError:
-        print("Количество строк должно быть задано положительным целым числом.")
-        return
-    try:
-        with open(file, 'r') as f:
-            all_lines = f.readlines()
-            last_lines = all_lines[-lines:]
-
-            for line in last_lines:
-                print(line.strip())
-    except FileNotFoundError:
-        print("Файл не найден.")
-
 '''
-
-
+#Цель задания - исследование функции 
+f(x)=(x^b+a^b)/x^b
+при различных значениях α и β. 
+Для каждой задачи необходимо: 
+Построить график (размер графика должен быть достаточным, чтобы визуально увидеть особенности изучаемых функций), график каждой функции должен быть одного цвета для одного значения α и β. 
+Подписать оси и заголовок 
+Создать легенду 
+Сохранить изображение в svg файл 
+Построить в общих осях графики для: 
+α=1,β=1 
+α=2,β=1
 '''
-Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на печать в терминал ее содержимое, как и всех подкаталогов при помощи функции print_docs(directory).
-'''
-
-
-'''
-import os
-
-def print_docs(directory):
-    for root, dirs, files in os.walk(directory):
-        level = root.replace(directory, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for file in files:
-            print('{}{}'.format(subindent, file))
-'''
-
-'''
-окумент article.txt содержит следующий текст:
-
->Вечерело  
- Жужжали мухи  
- Светил фонарик  
- Кипела вода в чайнике  
- Венера зажглась на небе  
- Деревья шумели  
- Тучи разошлись  
- Листва зеленела
-
-Требуется реализовать функцию `longest_words(file)`, которая выводит слово, имеющее максимальную длину (или список слов, если таковых несколько).  
-'''
-
-'''
-def longest_words(file):
-    try:
-        with open(file, 'r') as f:
-            content = f.read()
-            words = content.split()
-            max_length = len(max(words, key=len))
-            longest = [word for word in words if len(word) == max_length]
-            if len(longest) > 1:
-                print("Самые длинные слова в файле '{}' (длина {}):".format(file, max_length))
-                for word in longest:
-                    print(word)
-            else:
-                print("Самое длинное слово в файле '{}' (длина {}):".format(file, max_length))
-                print(longest[0])
-    except FileNotFoundError:
-        print("Файл не найден.")
-'''
-
-'''
-Составьте программу - простейший редактор текстовых файлов. 
-Алгоритм работы программы: 
-  1. Программа предлагает ввести имя будущего файла. Записывает его, присваивая расширение .txt. 
-  2. Программа предлагает записать строку в файл. При каждом нажатии кнопки ENTER происходит запись строки и переход на новую строчку. 
-  3. Если строка пустая или спец. символ - программа сохраняет файл.
-'''
-
-'''
-def text_editor():
-    file_name = input("Введите имя файла (без расширения): ")
-    file_name += ".txt"
-    try:
-        with open(file_name, 'w') as file:
-            while True:
-                line = input("Введите строку (или оставьте пустую строку для сохранения): ")
-                if line == "":
-                    print("Файл успешно сохранен.")
-                    break
-                file.write(line + "\n")
-    except FileNotFoundError:
-        print("Ошибка при открытии файла.")
-text_editor()
-'''
-
-'''
-Требуется создать csv-файл rows_300.csv со следующими столбцами:
-– № - номер по порядку (от 1 до 300);
-– Секунда – текущая секунда на вашем ПК;
-– Микросекунда – текущая миллисекунда на часах.
-На каждой итерации цикла искусственно приостанавливайте скрипт на 0,01 секунды.
-'''
+import matplotlib.pyplot as plt
+import numpy as np
+def f(x, a, b):
+    return (x**b + a**b) / x**b
+x = np.linspace(0.1, 10, 100)
+alpha_values = [1, 2]
+beta_values = [1, 2]
+for alpha in alpha_values:
+    for beta in beta_values:
+        y = f(x, alpha, beta)
+        plt.plot(x, y, label=f'α={alpha}, β={beta}')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('График функции f(x)')
+plt.legend()
+plt.savefig('output.svg', format='svg')
+plt.show()
 
 
 '''
-import csv
-import time
+#остроить в общих осях графики для x>0.
+На том же графике сделать 2 врезки, демонстрирующие поведение графиков на 2 интервалах:
 
-def create_csv():
-    with open('rows_300.csv', 'w', newline='') as csvfile:
-        fieldnames = ['№', 'Секунда', 'Микросекунда']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(1, 301):
-            current_time = time.time()
-            seconds = int(current_time)
-            microseconds = int((current_time - seconds) * 1000000)
-            writer.writerow({'№': i, 'Секунда': seconds, 'Микросекунда': microseconds})
-            time.sleep(0.01)
-create_csv()
+для малых x
+для больших x
+Необходимо продемонстрировать возможность (или невозможность) пересечений и стремление функций.
+
+Цвет линий на врезках и основном графике должен быть одинаковым для одних и тех же значений α и β.
+
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+a = 1
+b = 1
+x = np.linspace(0.01, 10, 100) 
+y = (x**b + a**b) / x**b  
+x_small = np.linspace(0.01, 0.1, 50)
+y_small = (x_small**b + a**b) / x_small**b
+plt.plot(x_small, y_small, '--', label='Малые значения x')
+x_large = np.linspace(10, 100, 50)
+y_large = (x_large**b + a**b) / x_large**b
+plt.plot(x_large, y_large, '--', label='Большие значения x')
+plt.plot(x, y, label=r'$\alpha=1, \beta=1$')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('График функции f(x) для α=1, β=1 с врезками')
+plt.legend()
+plt.savefig('plot_with_insets.svg')
+plt.show()
+
+
+
+'''
+#Построить в общих осях графики для x<0.
+На том же графике сделать 1 врезку, демонстрирующую поведение графиков при удалении x от 0 к −∞.
+
+Необходимо продемонстрировать возможность (или невозможность) пересечений и стремление функций. Так же нанесите на графики прямую f(x) = 0.
+
+Цвет линий на врезках и основном графике должен быть одинаковым для одних и тех же значений α и β.
 
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+a = 1
+b = 1
+x = np.linspace(-10, -0.01, 100)  
+y = (x**b + a**b) / x**b  
+plt.plot(x, y, label=r'$\alpha=1, \beta=1$, x < 0')
+plt.axhline(0, color='black', linestyle='--', label=r'$f(x) = 0$')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('График функции f(x) для α=1, β=1 с врезкой для x < 0')
+plt.legend()
+plt.savefig('plot_with_inset_negative_x.svg')
+plt.show()
+
+
 
 '''
-При помощи библиотеки Pillow в директории circles (создайте ее во время выполнения функции) нарисуйте и сохраните 100 кругов радиусом 300 пикселей случайных цветов в формате jpg на белом фоне (каждый круг - отдельный файл). Для этого напишите функцию circles_generator(num_of_circles=100).
+#Построить в общих осях графики для:
+α=1,β=0.5
+
+α=1,β=−0.5
+
+α=1,β=−1.5
+Сделайте выводы о поведении графиков, включая возрастание/убывание и выпуклость/вогнутость
+
+В результате выполнения предыдущей задачи, вы вероятно заметите, что все графики с α=1 проходят через общую точку (1, 2).
+
+Постройте в одном ряду 3 графика, чтобы убедиться в выводах, сделанных по результатам предыдущей задачи.
+
+Каждый график будет содержать 4 кривые. 2 общих:
+
+α=1,β=0 (в качестве цвета попробуйте использовать 'b--')
+
+α=1,β=−1 (в качестве цвета попробуйте использовать 'r--')
+И по 2 уникальных для каждого графика:
+
+α=1,β=0.5 и
+
+α=1,β=0.8
+
+α=1,β=−0.5 и
+
+α=1,β=−0.8
+
+α=1,β=−1.5 и
+
+α=1,β=−2.5
+
+Не забудьте добавить легенду на каждый график. Для этого может потребоваться вызвать метод legend() для каждого объекта осей.
 '''
 
-
-'''
-from PIL import Image, ImageDraw
-import random
-import os
-
-def circles_generator(num_of_circles=100):
-    if not os.path.exists("circles"):
-        os.makedirs("circles")
-    width, height = 600, 600
-    for i in range(num_of_circles):
-        image = Image.new("RGB", (width, height), "white")
-        draw = ImageDraw.Draw(image)
-        center_x = random.randint(0, width)
-        center_y = random.randint(0, height) 
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        draw.ellipse((center_x - 300, center_y - 300, center_x + 300, center_y + 300), fill=color)
-        image.save(f"circles/circle_{i+1}.jpg")
-circles_generator(100)
-'''
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(0.1, 10, 100)
+y_general_1 = (x**0 + 1**0) / x**0
+y_general_2 = (x**(-1) + 1**(-1)) / x**(-1)
+a1, b1 = 1, 0.5
+y1_unique_1 = (x**b1 + a1**b1) / x**b1
+a1, b1 = 1, 0.8
+y1_unique_2 = (x**b1 + a1**b1) / x**b1
+plt.subplot(131)
+plt.plot(x, y_general_1, 'b--', label=r'$\alpha=1, \beta=0$')
+plt.plot(x, y_general_2, 'r--', label=r'$\alpha=1, \beta=-1$')
+plt.plot(x, y1_unique_1, label=r'$\alpha=1, \beta=0.5$')
+plt.plot(x, y1_unique_2, label=r'$\alpha=1, \beta=0.8$')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title(r'График функции $f(x)$ для α=1, β=0.5')
+plt.legend(loc='upper left')
+a2, b2 = 1, -0.5
+y2_unique_1 = (x**b2 + a2**b2) / x**b2
+a2, b2 = 1, -0.8
+y2_unique_2 = (x**b2 + a2**b2) / x**b2
+plt.subplot(132)
+plt.plot(x, y_general_1, 'b--', label=r'$\alpha=1, \beta=0$')
+plt.plot(x, y_general_2, 'r--', label=r'$\alpha=1, \beta=-1$')
+plt.plot(x, y2_unique_1, label=r'$\alpha=1, \beta=-0.5$')
+plt.plot(x, y2_unique_2, label=r'$\alpha=1, \beta=-0.8$')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title(r'График функции $f(x)$ для α=1, β=-0.5')
+plt.legend(loc='upper right')
+a3, b3 = 1, -1.5
+y3_unique_1 = (x**b3 + a3**b3) / x**b3
+a3, b3 = 1, -2.5
+y3_unique_2 = (x**b3 + a3**b3) / x**b3
+plt.subplot(133)
+plt.plot(x, y_general_1, 'b--', label=r'$\alpha=1, \beta=0$')
+plt.plot(x, y_general_2, 'r--', label=r'$\alpha=1, \beta=-1$')
+plt.plot(x, y3_unique_1, label=r'$\alpha=1, \beta=-1.5$')
+plt.plot(x, y3_unique_2, label=r'$\alpha=1, \beta=-2.5$')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title(r'График функции $f(x)$ для α=1, β=-1.5')
+plt.legend(loc='upper right')
+plt.subplots_adjust(wspace=0.4)
+plt.savefig('multiple_graphs.svg')
+plt.show()
